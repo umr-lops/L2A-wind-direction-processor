5afa48ddba2414b76aa91f34e4012cfe051ace30
agrouaze <antoine.grouazel@gmail.com>
2023-11-08 17:14:45 +0100

    reformat

--- l2awinddirection/generate_L2A_winddir_pdf_product.py
+++ l2awinddirection/generate_L2A_winddir_pdf_product.py
@@ -11,23 +11,21 @@ inspired from generate_L1_wind_product_v2.ipynb (R Marquart)
 /raid/singularity-images/tensorflow-22.04.sif
 
 """
-import os
 import numpy as np
-import scipy as sp
-import pandas as pd
 import xarray as xr
-from scipy import stats
 import glob
-import os, sys
+import os
 
 from tqdm import tqdm
 
-# /!\ --------- /!\ ---------- /!\ #
-# /!\ ----- M64RN4 class ----- /!\ # 
-# /!\ --------- /!\ ---------- /!\ #
-sys.path.append('./../shared_codes')
-from M64RN4 import M64RN4_distribution
-def generate_wind_distribution_product(tiles, m64rn4, nb_classes = 36, shape = (44, 44, 1)):
+# /!\ --------- /!\ ---------- /!\ #
+# /!\ ----- M64RN4 class ----!\ #
+# !\ --------- /!\ ---------- /!\ #
+
+from l2awinddirection.M64RN4 import M64RN4_distribution
+
+
+def generate_wind_distribution_product(tiles, m64rn4, nb_classes=36, shape=(44, 44, 1)):
     """
     Generate wind direction from previously extracted tiles and store them in an xarray.Dataset.
     Parameters:
@@ -37,57 +35,62 @@ def generate_wind_distribution_product(tiles, m64rn4, nb_classes = 36, shape = (
     Returns:
         xarray.Dataset: dataset containing the tiles with associated wind direction.
     """
-    tiles_stacked = tiles.stack(all_tiles = ['burst', 'tile_line','tile_sample'])
-    tiles_stacked_no_nan = tiles_stacked.where(~np.any(np.isnan(tiles_stacked.sigma0), axis = (0, 1)), drop = True)
+    tiles_stacked = tiles.stack(all_tiles=['burst', 'tile_line', 'tile_sample'])
+    tiles_stacked_no_nan = tiles_stacked.where(~np.any(np.isnan(tiles_stacked.sigma0), axis=(0, 1)), drop=True)
     X = tiles_stacked_no_nan.sigma0.transpose('all_tiles', 'azimuth', 'range').values
-    X_normalized = np.array([((x - np.average(x))/np.std(x)).reshape(shape) for x in X]) # Normalize data
-    
-    heading_angle = np.deg2rad(tiles_stacked_no_nan['ground_heading'].values) 
+    X_normalized = np.array([((x - np.average(x)) / np.std(x)).reshape(shape) for x in X])  # Normalize data
 
-    # Assign new coordinates 'bin_centers' to the dataset
-    dx = 180/n_classes
-    angles = np.arange(0 + dx/2, 180 + dx/2, dx)
+    heading_angle = np.deg2rad(tiles_stacked_no_nan['ground_heading'].values)
 
-    tiles_stacked_no_nan = tiles_stacked_no_nan.assign_coords(bin_centers = ('bin_centers', angles))
+    # Assign new coordinates 'bin_centers' to the dataset
+    dx = 180 / nb_classes
+    angles = np.arange(0 + dx / 2, 180 + dx / 2, dx)
 
-    # Predict wind direction distributions and add them to the dataset
+    tiles_stacked_no_nan = tiles_stacked_no_nan.assign_coords(bin_centers=('bin_centers', angles))
+
+    # Predict wind direction distributions and add them to the dataset
     predictions = np.squeeze(m64rn4.model.predict(X_normalized))
-    tiles_stacked_no_nan = tiles_stacked_no_nan.assign(wind_direction_distribution = (['all_tiles', 'bin_centers'],  predictions))
+    tiles_stacked_no_nan = tiles_stacked_no_nan.assign(
+        wind_direction_distribution=(['all_tiles', 'bin_centers'], predictions))
 
-    # Compute mean wind direction, most likely wind direction and associated standard deviation 
-    mean_wdir = xr.apply_ufunc(compute_mean_direction, tiles_stacked_no_nan.wind_direction_distribution, tiles_stacked_no_nan.bin_centers,
+    # Compute mean wind direction, most likely wind direction and associated standard deviation
+    mean_wdir = xr.apply_ufunc(compute_mean_direction, tiles_stacked_no_nan.wind_direction_distribution,
+                               tiles_stacked_no_nan.bin_centers,
                                input_core_dims=[['bin_centers'], ['bin_centers']],
                                vectorize=True
                                )
-    
-    most_likely_wdir = xr.apply_ufunc(compute_most_likely_direction, tiles_stacked_no_nan.wind_direction_distribution, tiles_stacked_no_nan.bin_centers,
+
+    most_likely_wdir = xr.apply_ufunc(compute_most_likely_direction, tiles_stacked_no_nan.wind_direction_distribution,
+                                      tiles_stacked_no_nan.bin_centers,
                                       input_core_dims=[['bin_centers'], ['bin_centers']],
                                       vectorize=True
                                       )
 
-    std_wdir = xr.apply_ufunc(compute_standard_deviation, tiles_stacked_no_nan.wind_direction_distribution, tiles_stacked_no_nan.bin_centers,
+    std_wdir = xr.apply_ufunc(compute_standard_deviation, tiles_stacked_no_nan.wind_direction_distribution,
+                              tiles_stacked_no_nan.bin_centers,
                               input_core_dims=[['bin_centers'], ['bin_centers']],
                               vectorize=True
                               )
-    
+
     # Format dataset
-    tiles_stacked_no_nan = tiles_stacked_no_nan.assign(mean_wdir = (['all_tiles'], mean_wdir.data))
+    tiles_stacked_no_nan = tiles_stacked_no_nan.assign(mean_wdir=(['all_tiles'], mean_wdir.data))
     tiles_stacked_no_nan['mean_wdir'].attrs['definition'] = '180° ambiguous mean wind direction.'
     tiles_stacked_no_nan['mean_wdir'].attrs['long_name'] = 'Mean wind direction'
     tiles_stacked_no_nan['mean_wdir'].attrs['convention'] = 'Clockwise, relative to geographic North.'
     tiles_stacked_no_nan['mean_wdir'].attrs['units'] = 'degree'
     tiles_stacked_no_nan['mean_wdir'].attrs['vmin'] = 0
     tiles_stacked_no_nan['mean_wdir'].attrs['vmax'] = 180
-    
-    tiles_stacked_no_nan = tiles_stacked_no_nan.assign(most_likely_wdir = (['all_tiles'], most_likely_wdir.data))
-    tiles_stacked_no_nan['most_likely_wdir'].attrs['definition'] = '180° ambiguous most likely wind direction, defined with a bin precision of 2.5°.'
+
+    tiles_stacked_no_nan = tiles_stacked_no_nan.assign(most_likely_wdir=(['all_tiles'], most_likely_wdir.data))
+    tiles_stacked_no_nan['most_likely_wdir'].attrs[
+        'definition'] = '180° ambiguous most likely wind direction, defined with a bin precision of 2.5°.'
     tiles_stacked_no_nan['most_likely_wdir'].attrs['long_name'] = 'Most likely wind direction'
     tiles_stacked_no_nan['most_likely_wdir'].attrs['convention'] = 'Clockwise, relative to geographic North.'
     tiles_stacked_no_nan['most_likely_wdir'].attrs['units'] = 'degree'
     tiles_stacked_no_nan['most_likely_wdir'].attrs['vmin'] = 0
     tiles_stacked_no_nan['most_likely_wdir'].attrs['vmax'] = 180
-    
-    tiles_stacked_no_nan = tiles_stacked_no_nan.assign(std_wdir = (['all_tiles'], std_wdir.data))
+
+    tiles_stacked_no_nan = tiles_stacked_no_nan.assign(std_wdir=(['all_tiles'], std_wdir.data))
     tiles_stacked_no_nan['std_wdir'].attrs['definition'] = 'Standard deviation associated with wind direction.'
     tiles_stacked_no_nan['std_wdir'].attrs['long_name'] = 'Standard deviation of the wind direction'
     tiles_stacked_no_nan['std_wdir'].attrs['units'] = 'degree'
@@ -95,8 +98,8 @@ def generate_wind_distribution_product(tiles, m64rn4, nb_classes = 36, shape = (
     tiles_stacked_no_nan['std_wdir'].attrs['vmax'] = 180
 
     return tiles_stacked_no_nan.unstack()
-                                                       
-                                                       
+
+
 def compute_mean_direction(weights, angles):
     """
     Compute mean direction given an array of weights and angles taking into account the circularity between 0 and 180° of the angles.
@@ -104,15 +107,15 @@ def compute_mean_direction(weights, angles):
         weights (numpy.ndarray): probability corresponding to each angle.
         angles (numpy.ndarray): array of angles in degrees corresponding to the middle of the bins used for training (e.g.: [0,5] -> 2.5).
     Returns:
-        float: Computed mean direction. 
+        float: Computed mean direction.
     """
-    angles_rad = angles*(2*np.pi)/180 # Recast angles as radians that range between 0 and 2 pi
-    s_a = np.sum(weights*np.sin(angles_rad))
-    c_a = np.sum(weights*np.cos(angles_rad))
-    
+    angles_rad = angles * (2 * np.pi) / 180  # Recast angles as radians that range between 0 and 2 pi
+    s_a = np.sum(weights * np.sin(angles_rad))
+    c_a = np.sum(weights * np.cos(angles_rad))
+
     mean_direction = np.arctan2(s_a, c_a)
-    
-    return mean_direction*180/(2*np.pi) # Convert back the result to the considered range
+
+    return mean_direction * 180 / (2 * np.pi)  # Convert back the result to the considered range
 
 
 def compute_standard_deviation(weights, angles):
@@ -122,16 +125,16 @@ def compute_standard_deviation(weights, angles):
         weights (numpy.ndarray): probability corresponding to each angle.
         angles (numpy.ndarray): array of angles in degrees corresponding to the middle of the bins used for training (e.g.: [0,5] -> 2.5).
     Returns:
-        float: Computed standard deviation. 
+        float: Computed standard deviation.
     """
-    mean_dir = compute_mean_direction(weights, angles) 
-    
+    mean_dir = compute_mean_direction(weights, angles)
+
     A = np.abs(angles - mean_dir)
     B = np.abs(180 - np.abs(angles - mean_dir))
-    # Select the min difference between the two terms defined above to deal with 180° discontinuity
-    delta = np.min(np.vstack([A, B]), axis = 0) # 180° ambiguity
-    variance = np.sum(weights*delta**2)
-    
+    #  Select the min difference between the two terms defined above to deal with 180° discontinuity
+    delta = np.min(np.vstack([A, B]), axis=0)  # 180° ambiguity
+    variance = np.sum(weights * delta ** 2)
+
     return np.sqrt(variance)
 
 
@@ -142,13 +145,13 @@ def compute_most_likely_direction(weights, angles):
         weights (numpy.ndarray): probability corresponding to each angle.
         angles (numpy.ndarray): array of angles in degrees corresponding to the middle of the bins used for training (e.g.: [0,5] -> 2.5).
     Returns:
-        float: Most likely direction corresponding to the highest probability. 
+        float: Most likely direction corresponding to the highest probability.
     """
     return angles[np.argmax(weights)]
 
 
 def main():
-    # Parameters to instantiante the models
+    #  Parameters to instantiante the models
     input_shape = (44, 44, 1)
     data_augmentation = True
     learning_rate = 1e-3
@@ -165,7 +168,7 @@ def main():
         tiles = xr.open_dataset(file)
         if len(tiles.variables.keys()) == 0:
             continue
-        res = generate_wind_distribution_product(tiles, model_m64rn4,nb_classes = 36, shape = (44, 44, 1))
+        res = generate_wind_distribution_product(tiles, model_m64rn4, nb_classes=36, shape=(44, 44, 1))
         res.to_netcdf(file.replace('.nc', '_wind.nc'))
         # if you want to remove the file containing the tiles used for inference (data kept in final product)
         del tiles
